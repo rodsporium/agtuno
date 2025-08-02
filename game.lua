f11009b21a752b0292b8476c23368ed31f53e24e
rodsporium <rodsvilla@gmail.com>
2025-08-02 16:28:58 +0800

    start again from scratch. establish a similar structure with balatro

--- game.lua
+++ game.lua
@@ -1,150 +1,33 @@
 -- game.lua
 -- This is the main game logic controller.
 
--- We 'extend' the base Object to create our Game class
-Game = Object:extend()
-
--- The constructor for our Game class
-function Game:init()
-    G = self
-
-    self:set_globals()
+function start_up()
+    set_globals()
 end
 
--- This function is called once at the very beginning of the game
-function Game:start_up()
-    -- This function loads assets and prepares the game window
-    self:set_render_settings()
-    self.E_MANAGER = EventManager()
-    -- This function starts the game's title sequence
-    self:splash_screen()
+function update(dt)
+    movement(dt)
 end
 
--- The main update loop, called every frame by main.lua
-function Game:update(dt)
-    -- Update the Event Manager first, as it controls animations and delays
-    self.E_MANAGER:update(dt)
+function draw()
+    draw_circle()
+end
 
-    -- Update all moveable objects
-    for k, v in pairs(G.MOVEABLES) do
-        v:move(dt)
+function movement(dt)
+    if love.keyboard.isDown("right") then
+        player.x = player.x + 100 * dt
+    end
+    if love.keyboard.isDown("left") then
+        player.x = player.x - 100 * dt
+    end
+    if love.keyboard.isDown("up") then
+        player.y = player.y - 100 * dt
+    end
+    if love.keyboard.isDown("down") then
+        player.y = player.y + 100 * dt
     end
 end
 
--- The main draw loop, called every frame by main.lua
-function Game:draw()
-    love.graphics.clear(G.C.BLACK)
-
-    -- For the splash screen and main menu, we draw the specific background and logo objects directly.
-    if self.STATE == self.STATES.SPLASH or self.STATE == self.STATES.MENU then
-        if self.SPLASH_BACK then self.SPLASH_BACK:draw() end
-        if self.SPLASH_LOGO then self.SPLASH_LOGO:draw() end
-    end
-
-    -- Draw the menu text if the flag is set
-    if self.show_menu_text then
-        love.graphics.setFont(love.graphics.newFont(36))
-        love.graphics.printf("Press Enter to Start", 0, 450, 800, 'center')
-    end
-end
-
--- A simplified version of Balatro's asset loading function
-function Game:set_render_settings()
-    self.ASSET_ATLAS = {}
-    -- NOTE: This requires the file 'resources/textures/1x/balatro.png' to exist
-    if love.filesystem.getInfo("resources/textures/1x/balatro.png") then
-        self.ASSET_ATLAS["balatro"] = {
-            image = love.graphics.newImage("resources/textures/1x/balatro.png"),
-            px = 333, py = 216
-        }
-    else
-        -- Create a placeholder if the image is missing
-        print("WARNING: 'resources/textures/1x/balatro.png' not found. Creating placeholder.")
-        local canvas = love.graphics.newCanvas(333, 216)
-        love.graphics.setCanvas(canvas)
-        love.graphics.clear(G.C.RED)
-        love.graphics.setCanvas()
-        self.ASSET_ATLAS["balatro"] = { image = canvas, px = 333, py = 216 }
-    end
-end
-
--- Prepares the stage for a new scene
-function Game:prep_stage(new_stage, new_state)
-    self.STAGE = new_stage or G.STAGES.MAIN_MENU
-    self.STATE = new_state or G.STATES.MENU
-    
-    -- G.ROOM is the main container for all game objects
-    self.ROOM = Node{T={x = 0, y = 0, w = G.TILE_W, h = G.TILE_H}}
-end
-
--- This is the function that starts the title screen animation
-function Game:splash_screen()
-    self:prep_stage(G.STAGES.MAIN_MENU, G.STATES.SPLASH)
-    
-    self.E_MANAGER:add_event(Event({
-        trigger = 'immediate',
-        func = function()
-            G.TIMERS.TOTAL = 0
-            G.TIMERS.REAL = 0
-            
-            -- Go to the main menu after a short delay
-            G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.1, func = function()
-                G:main_menu('splash')
-                return true
-            end}))
-            return true
-        end
-    }))
-end
-
--- This function sets up the main menu scene
-function Game:main_menu(change_context)
-    self:prep_stage(G.STAGES.MAIN_MENU, G.STATES.MENU)
-
-    -- Create the background and logo sprites
-    self.SPLASH_BACK = Sprite(-30, -13, G.ROOM.T.w+60, G.ROOM.T.h+22, G.ASSET_ATLAS["balatro"], {x=0,y=0})
-    self.SPLASH_LOGO = Sprite(0, 0, 13, 13 * (216/333), G.ASSET_ATLAS["balatro"], {x=0, y=0})
-    
-    self.SPLASH_LOGO:set_alignment({ major = G.ROOM, type = 'cm', offset = {x=0, y=-2} })
-    self.SPLASH_LOGO.dissolve = 1 -- Start invisible
-
-    -- Use the event manager to fade in the logo
-    self.E_MANAGER:add_event(Event({
-        trigger = 'after',
-        delay = 0.5,
-        func = function()
-            ease_value(G.SPLASH_LOGO, 'dissolve', -1, nil, nil, nil, 1.5)
-            return true
-        end
-    }))
-
-    -- Use the event manager to show the menu text after the logo
-    self.E_MANAGER:add_event(Event({
-        trigger = 'after',
-        delay = 2.5,
-        func = function()
-            set_main_menu_UI()
-            return true
-        end
-    }))
-end
-
--------------------------------------------------
--- HELPER FUNCTIONS
--------------------------------------------------
-
--- This function will eventually create the button UI. For now, it just sets a flag.
-function set_main_menu_UI()
-    G.show_menu_text = true
-end
-
--- This function creates an 'ease' event to smoothly animate a value over time.
-function ease_value(ref_table, ref_value, ease_to, _, _, _, duration)
-    G.E_MANAGER:add_event(Event({
-        trigger = 'ease',
-        ref_table = ref_table,
-        ref_value = ref_value,
-        ease_to = ref_table[ref_value] + ease_to,
-        delay = duration or 1
-    }))
+function draw_circle()
+    love.graphics.circle("fill",player.x,player.y,100)
 end
--- main.lua
+++ main.lua
@@ -2,29 +2,21 @@
 -- This is the main entry point for the LÖVE2D game.
 
 -- Load the foundational files first
-require 'engine/object'
-require "engine/event"
-require "engine/node"
-require "engine/moveable"
-require "engine/sprite"
-require "functions/misc_functions"
 require "game"
 require "globals"
 
 -- The love.load() function is called only once at the start of the game.
 function love.load()
-    G:start_up()
+    start_up()
 end
 
 -- The love.update() function is called every frame.
 -- 'dt' is the delta time, or the time since the last frame.
 function love.update(dt)
-    -- We simply delegate the update call to our Game object.
-    G:update(dt)
+    update(dt)
 end
 
 -- The love.draw() function is called every frame after love.update().
 function love.draw()
-    -- We simply delegate the draw call to our Game object.
-    G:draw()
+    draw()
 end
